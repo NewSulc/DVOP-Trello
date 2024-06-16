e1c90c79661e53846a8b30b5b10c3f42ccc939c6
NewSulc <vsulc007@outlook.cz>
2024-06-16 19:11:13 +0200

    List routing

--- dvop-trello-server/server.js
+++ dvop-trello-server/server.js
@@ -1,15 +1,11 @@
 import express from 'express';
 import cors from 'cors';
 import bodyParser from 'body-parser';
-import prisma from './prismaClient.js';
-
-import { boards } from './data.js';
 
 import userRoutes from './routes/User.js';
 import projectRoutes from './routes/Project.js';
 import boardRoutes from './routes/Board.js';
-
-import { authenticateToken } from './token.js'
+import listRoutes from './routes/List.js';
 
 const app = express();
 
@@ -25,34 +21,7 @@ app.use("/project", projectRoutes)
 
 app.use("/board", boardRoutes)
 
-app.get("/list/:listId", authenticateToken, async (req, res) => {
-    try {
-        const lists = await prisma.task.findMany({
-            where: {
-                list_id: Number(req.params.listId)
-            }
-        })
-
-        res.send(lists)
-    } catch {
-        res.status(500).send()
-    }
-});
-
-app.post("/list/:listId/task", authenticateToken, async (req, res) => {
-    try {
-        const lists = await prisma.task.create({
-            data: {
-                name: req.body.name,
-                list_id: Number(req.params.listId)
-            }
-        });
-
-        res.send(lists)
-    } catch {
-        res.status(500).send()
-    }
-});
+app.use("/list", listRoutes)
 
 app.listen(8080, () => {
     console.log("Listening on 8080");
